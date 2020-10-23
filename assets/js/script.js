fca28a2511ab2c78ee3e02ca67778ea635b2ca84
Lydia-tech <71572534+Lydia-tech@users.noreply.github.com>
2020-10-23 12:01:22 -0500

    refactored add a tasktest errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
@@ -24,20 +25,68 @@ formEl.reset();
   createTaskEl(taskDataObj);
 }
 var createTaskEl = function(taskDataObj) {
-// create list item
-var listItemEl = document.createElement("li");
-listItemEl.className = "task-item";
+  // create list item
+  var createTaskActions = function(taskId) {  
+  var actionContainerEl = document.createElement("div");
+  actionContainerEl.className = "task-actions"; 
+  
+  
+// create edit button
+var editButtonEl = document.createElement("button");
+editButtonEl.textContent = "Edit";
+editButtonEl.className = "btn edit-btn";
+editButtonEl.setAttribute("data-task-id", taskId);
 
-// create div to hold task info and add to list item
-var taskInfoEl = document.createElement("div");
-taskInfoEl.className = "task-info";
-taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+actionContainerEl.appendChild(editButtonEl);
 
-listItemEl.appendChild(taskInfoEl);
+// create delete button
+var deleteButtonEl = document.createElement("button");
+deleteButtonEl.textContent = "Delete";
+deleteButtonEl.className = "btn delete-btn";
+deleteButtonEl.setAttribute("data-task-id", taskId);
 
-// add entire list item to list
-tasksToDoEl.appendChild(listItemEl);
+actionContainerEl.appendChild(deleteButtonEl); 
+var statusSelectEl = document.createElement("select");
+statusSelectEl.className = "select-status";
+statusSelectEl.setAttribute("name", "status-change");
+statusSelectEl.setAttribute("data-task-id", taskId);
+var statusChoices = ["To Do", "In Progress", "Completed"];
 
+for (var i = 0; i < statusChoices.length; i++) {
+  // create option element
+  var statusOptionEl = document.createElement("option");
+  statusOptionEl.textContent = statusChoices[i];
+  statusOptionEl.setAttribute("value", statusChoices[i]);
 
+  // append to select
+  statusSelectEl.appendChild(statusOptionEl);
 }
+
+
+actionContainerEl.appendChild(statusSelectEl);
+return actionContainerEl;
+  };
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+
+  // add task id as a custom attribute
+  listItemEl.setAttribute("data-task-id", taskIdCounter);
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+  console.log(list)
+  // add entire list item to list
+  
+  var taskActionsEl = createTaskActions(taskIdCounter);
+  console.log(taskActionsEl);
+
+  tasksToDoEl.appendChild(listItemEl);
+
+  // increase task counter for next unique id
+  taskIdCounter++;
+};
+
 formEl.addEventListener("submit", taskFormHandler); 
